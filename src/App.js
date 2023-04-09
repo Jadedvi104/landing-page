e1d78b2d7ca25ab6544ed634d731330be6af34fd
jirayu.n@echoplus.io <jirayu.n@echoplus.io>
2023-04-09 12:16:16 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-black">
       <header>
         <Navbar />
       </header>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Durian from "../images/durian-yellow-brand.png";
 
 export const Navbar = () => {
   return (
-    <div className='text-black'>
-        <h1>REACT TEST</h1>
+    <div   className="flex bg-lime-300">
+      <div class="py-2 flex-none">
+        <img src={Durian} />
+      </div>
+      <div id="menus" class="flex-auto w-32 lg:w-48">
+        <ul className="flex">
+          <li className="p-4">คอร์สเรียน</li>
+          <li className="p-4">ร้านหนังสือ</li>
+          <li className="p-4">คลังข้อสอบ</li>
+          <li className="p-4">คลังความรู้</li>
+          <li className="p-4">เกี่ยวกับเรา</li>
+        </ul>
+      </div>
+      <div id="menus" class="py-2 flex-none">
+      <img src={Durian} />
+      </div>
     </div>
-  )
-}
+  );
+};
